78d2826f4ab34f7400ec13d4b1cc3d038523777c
Jayant Apte <jayantapte34@gmail.com>
2025-09-03 02:15:49 +0530

    cinemate #2

--- cinemate/src/components/Footer.js
+++ cinemate/src/components/Footer.js
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom"
 
 export const Footer = () => {
   return (
-    <footer className="bg-white border-t my-4 dark:bg-gray-800">
-      <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+    <footer className="bg-white border-t mt-4 dark:bg-gray-800">
+      <div className="w-full mx-auto max-w-screen-xl p-7 md:flex md:items-center md:justify-between">
         <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <Link to="/" className="hover:underline">Cineflix</Link>. All Rights Reserved.
         </span>
         <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
